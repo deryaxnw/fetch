1221bb1f123a8b001084e107c79f795dd629c438
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-06 09:38:50 -0300

    async await

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 
 
 
-fetch("https://viacep.com.br/ws/01001000/json/").then((response) => {
-    console.log(response)
-}).catch((error) => {
-    console.log(error)
-})
+// fetch("https://viacep.com.br/ws/01001000/json/").then((response) => {
+//     console.log(response)
+
+//         response.json().then((data) => console.log(data))
+
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+
+async function integrar() {
+    const response = await fetch("https://viacep.com.br/ws/01001000/json/")
+
+    const data = await response.json()
+
+    console.log(data)
+}
+
+integrar()
