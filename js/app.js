01482a8e675b449b74e569e84ff771feb051c0b1
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-07 19:01:53 -0300

    fetch de api cep concluida

--- js/app.js
+++ js/app.js
@@ -1,22 +1,54 @@
+const form = document.getElementById("data");
 
 
-
-// fetch("https://viacep.com.br/ws/01001000/json/").then((response) => {
-//     console.log(response)
-
-//         response.json().then((data) => console.log(data))
-
-// }).catch((error) => {
-//     console.log(error)
-// })
-
-
-async function integrar() {
-    const response = await fetch("https://viacep.com.br/ws/01001000/json/")
-
-    const data = await response.json()
-
-    console.log(data)
+window.onload = () => {
+    colocarFoco(form.cep);
 }
 
-integrar()
+
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const cep = form.cep.value; // Captura o valor do campo "cep"
+
+    if (cep.length !== 8) {
+        return;
+    }
+
+    await buscar(cep); // Passa o CEP para a função buscar
+});
+
+async function buscar(cep) {
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        
+
+        const dados = await response.json();
+
+        // Preenche os campos com os dados retornados
+
+        form.logradouro.value = dados.logradouro;
+        form.bairro.value = dados.bairro;
+        form.localidade.value = dados.localidade;
+        form.estado.value = dados.estado; 
+        form.regiao.value = dados.regiao;
+        
+    } catch (error) {
+        console.error(error);
+    }
+
+        // Limpa o campo de CEP
+        limparCEP();
+
+        // Coloca o foco no campo Número
+        colocarFoco(form.numero);
+}
+
+function limparCEP() {
+    form.cep.value = '';
+}
+
+function colocarFoco(campo) {
+    campo.focus();
+}
